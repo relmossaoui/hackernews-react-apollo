6d9b2e8fcc8e6f55134cac90aafbe265432e45fc
relmossaoui <redouan.elmossaoui2@gmail.com>
2020-01-14 23:30:17 +0100

    connect GraphQL server to database using prisma

--- back/src/index.js
+++ back/src/index.js
@@ -1,68 +1,38 @@
 
 const { GraphQLServer } = require('graphql-yoga');
 
-let { links } = require('./mock')
-
 const { prisma } = require('./generated/prisma-client')
 
-async function main() {
-
-  // Create a new link
-  const newLink = await prisma.createLink({ 
-    url: 'www.prisma.io',
-    description: 'Prisma replaces traditional ORMs',
-  })
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
-
-  // Read all links from the database and print them to the console
-  const allLinks = await prisma.links()
-  console.log(allLinks)
-}
-
-main().catch(e => console.error(e))
-
 // GraphQL Resolvers
-let idCount = links.length;
 const resolvers = {
     Query : {
         info : () => 'This is the API of a Hackernews Clone',
-        feed : () => links,
-        link : (parent, args) => links.find(link => link.id === args.id)
+        feed : () => prisma.links(),
+        link : (parent, args) => prisma.link({
+            id : args.id
+        }) 
     },
 
     Mutation : {
         post(parent, args) {
-            const link = {
-                id: `link-${idCount++}`,
+
+            return prisma.createLink({
                 description : args.description,
                 url: args.url
-            }
-
-            links.push(link)
-
-            return link
+            })
         },
 
-        updateLink(parent, args) {
-            let link = links.find(link => link.id === args.id)
+        async updateLink(parent, args) {
+            let data = {}
 
-            if (!link) return null
+            if (args.url) data.url = args.url
+            if (args.description) data.description = args.description
 
-            if (args.url) link.url = args.url
-            if (args.description) link.description = args.description
-
-            return link
+            return prisma.updateLink({data, where: { id: args.id}})
         },
 
         deleteLink(parent, args) {
-            let index = links.findIndex(link => link.id === args.id);
-            let link = null;
-            if (index > -1) {
-                link = links[index]
-                links.splice(index, 1)
-            }
-
-            return link
+            return prisma.deleteLink({id: args.id})
         }
     },
 
